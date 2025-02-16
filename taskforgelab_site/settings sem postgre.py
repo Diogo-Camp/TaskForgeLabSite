ddf03f6e9e3b49ec90f7d704ab5449afee2eed45
Diogo-Camp <diogo.campos2@gmail.com>
2025-02-16 04:56:05 -0300

    suporte a postgresql e .env adicionado

--- taskforgelab_site/settings sem postgre.py
+++ taskforgelab_site/settings sem postgre.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
-from dotenv import load_dotenv
+
 from pathlib import Path
 import os
-
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'django-insecure-ap(wfp4*1i_7$$fymf@z*(t2ber8i)3xo(!d0e@-gh*44+9_h('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,15 +78,10 @@ WSGI_APPLICATION = 'taskforgelab_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
@@ -134,6 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para onde `collectstatic`
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-DEBUG = os.getenv('DEBUG') == 'True'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'core/templates')]
